c220e6be3eea96af76fa20d7851386ad47b4d9cb
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2025-08-24 00:24:10 +0530

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -3,3 +3,5 @@ import pyttsx3
 engine = pyttsx3.init()
 engine.say("i will speak this text")
 engine.runAndWait()
+
+#make sure audio is on of laptop its module which will speak loud whatever you written in engine.say
