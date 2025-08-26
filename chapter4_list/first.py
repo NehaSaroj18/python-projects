a3489dc50e5e83ee20dbebf7f0b6d06fcf4dced4
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2025-08-26 11:24:16 +0530

    Renamed file

--- chapter4_list/first.py
+++ chapter4_list/first.py
@@ -1,4 +1,7 @@
 friends=["Apple","Orange",5,345.06,False,"Akash","Rohan"]
 print(friends[0])
-friends[0]="Grapes"
+friends[0]="Grapes" #list are mutable can be update and change
 print(friends[0])
+print(friends[1:4])# as indexing is working in strings in same way is works in 
+
+print(friends) #now in friend list in place of apple grapes will show that means this change the list
