38f69acf607ebaa99b8febe3c5a31dfc8fe33c8d
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2025-08-26 11:24:58 +0530

    Renamed file

---
+++ chapter3_string/immutable.py
@@ -0,0 +1,9 @@
+name="kanha"
+
+print(len(name))
+print(name.endswith("naa"))
+print(name.startswith("ka"))
+print(name.capitalize())
+
+#string is immutable in this all case means even though we check condition or capitalize
+#name but this will not change 
