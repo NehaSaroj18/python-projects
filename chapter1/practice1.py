01d282644dfd90a6f1b6685b752d72707f10a021
NehaSaroj18 <218978146+NehaSaroj18@users.noreply.github.com>
2025-08-27 23:19:43 +0530

    recent update

--- chapter1/practice1.py
+++ chapter1/practice1.py
@@ -1,7 +1,7 @@
 # 3. Install an external module and use it to perform an operation of your interest.  
 import pyttsx3
 engine = pyttsx3.init()
-engine.say()
+engine.say("hello world")
 engine.runAndWait()
 
 #make sure audio is on of laptop its module which will speak loud whatever you written in engine.say
@@ -9,17 +9,17 @@ engine.runAndWait()
 # 4. Write a python program to print the contents of a directory using the os module. 
 # Search online for the function which does that.  
 
-import os
+# import os
 
 #specify the directory you want to list
-directory_path = '/'
+# directory_path = '/'
 
 #list all files and directories in the specified path
-contents=os.listdir(directory_path)
+# contents=os.listdir(directory_path)
 
 #print each file and directory name 
-for item in contents:
-    print(item)
+# for item in contents:
+#     print(item)
 
 #list all the files in directory by import os and using listdir
 
